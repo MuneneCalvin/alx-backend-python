d31bf4906d52db53aa691ffeabfb89ccdf1630d5
MuneneCalvin <munenecalvn@gmail.com>
2023-04-27 13:48:58 +0300

    1. Basic annotations - concat

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+"""Concat module with type annotation"""
+
+def concat(str1: str, str2: str) -> str:
+    """Concats two strings and returns the concatenated string"""
+    return str1 + str2
