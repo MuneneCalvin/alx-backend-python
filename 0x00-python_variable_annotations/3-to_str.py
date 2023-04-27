6d1cab50c2f1efba3b839a3bad626523340db36f
MuneneCalvin <munenecalvn@gmail.com>
2023-04-27 13:50:31 +0300

    3. Basic annotations - to string

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+"""to_Str module with type annotation"""
+
+def to_str(n: float) -> str:
+    """Returns the string representation of a floating point number n"""
+    return str(n)
