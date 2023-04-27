ec925581ece418ec27b28acb4bad6804c0668bfc
MuneneCalvin <munenecalvn@gmail.com>
2023-04-27 13:48:26 +0300

    0. Basic annotations - add

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+"""Sum module with type annotation"""
+
+def add(a: float, b: float) -> float:
+    """Adds two floating point numbers and returns the result"""
+    return a + b
